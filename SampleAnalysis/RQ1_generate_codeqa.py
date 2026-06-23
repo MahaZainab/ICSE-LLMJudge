c143c1305b5ba59e1e1872289fe5c2e1ff629970
MahaZainab <mzainab.msit16seecs@seecs.edu.pk>
2026-06-23 14:44:33 -0500

    Auto commit on save

--- SampleAnalysis/RQ1_generate_codeqa.py
+++ SampleAnalysis/RQ1_generate_codeqa.py
@@ -6,9 +6,9 @@ import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-GENERATOR_MODEL_ID = "meta-llama/Llama-3.2-3B-Instruct"
+GENERATOR_MODEL_ID = "meta-llama/Llama-3.2-3B"
 INPUT_FILE         = "difficult_codeqa_selected.json"
-OUTPUT_FILE        = "RQ1_generate_codeqa_difficult_samples_predictions.json"
+OUTPUT_FILE        = "RQ1_codeqa_difficult_samples_predictions.json"
 SAVE_EVERY         = 25
 MAX_NEW_TOKENS     = 128
 HF_CACHE           = os.getenv("HF_HOME", "")
